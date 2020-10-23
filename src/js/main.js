b31b5faee27e718395ff7f74d1f2941d45b144f6
alicia-colom <aliciacolom@gmail.com>
2020-10-23 23:36:36 +0200

    Add favorites HTML painting from JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 'use strict';
 
-// [] 3.- boton RESET en búsqueda --> que aparezca cuando se haga la primera búsqueda
-//        (añadiendo o quitando la clase HIDDEN con un IF)
-// [] 4.- borrar búsqueda al realizar una búsqueda nueva
+// [] 2.- eliminar FAVs clicando sobre ellas
+// [] 3.- boton RESET en búsqueda --> que borre las series que aparecen en TU BUSQUEDA
 // [] 5.- event.currentTarget en el boton de eliminar las pelis de favoritos
 // [] 6.- si no hay favoritos, mostrar el texto "no tienes favoritos"
 
@@ -12,7 +11,7 @@
 let searchArray = [];
 let favArray = [];
 
-// OBJETOS PARA OPERAR:
+// OBJETOS CONSTRUCTORES PARA OPERAR:
 function searchItem(id, name, img) {
 	this.showId = id; // = dataAPI.show.id
 	this.showName = name; // = dataAPI.show.name
@@ -28,36 +27,49 @@ function favItem(id, name, img, alt, title) {
 
 const btnSearch = document.querySelector('.js-searchBtn');
 const textAreaSearch = document.querySelector('.js-searchTextArea');
-const containerSearch = document.querySelector('.js-searchList');
+const resetBtnSearch = document.querySelector('.js-searchReset');
+const warningSearch = document.querySelector('.js-searchWarning');
 
 // FUNCION PARA ENVÍO DE DATOS AL API:
 function handleSearch() {
 	searchArray = [];
 	const textSearch = textAreaSearch.value;
-	fetch(`//api.tvmaze.com/search/shows?q=${textSearch}`)
-		.then((response) => response.json())
-		.then((dataAPIList) => {
-			for (let i = 0; i < dataAPIList.length; i++) {
-				const idFromAPI = dataAPIList[i].show.id;
-				const nameFromAPI = dataAPIList[i].show.name;
-				const imgFromAPI = existMediumImg(dataAPIList[i].show.image);
-				searchArray.push(new searchItem(idFromAPI, nameFromAPI, imgFromAPI));
-			}
-			paintSearch();
-			addFavEvent();
-		});
+	if (!textSearch) {
+		warningSearch.classList.remove('hidden');
+	} else {
+		warningSearch.classList.add('hidden');
+		resetBtnSearch.classList.remove('hidden');
+		fetch(`//api.tvmaze.com/search/shows?q=${textSearch}`)
+			.then((response) => response.json())
+			.then((dataAPIList) => {
+				for (let i = 0; i < dataAPIList.length; i++) {
+					const idFromAPI = dataAPIList[i].show.id;
+					const nameFromAPI = dataAPIList[i].show.name;
+					const imgFromAPI = existMediumImg(dataAPIList[i].show.image);
+					searchArray.push(new searchItem(idFromAPI, nameFromAPI, imgFromAPI));
+				}
+				paintSearch();
+				addFavEvent();
+			});
+	}
 }
 
 // FUNCION PARA COMPROBAR SI EXISTE IMAGE.MEDIUM EN LA API:
 function existMediumImg(photo) {
 	if (photo !== null && photo.medium !== null) {
 		return photo.medium;
+	} else {
+		return '//via.placeholder.com/210x295/e65027/800080/?text=¿?';
+		//'../../assets/images/carta_ajuste.png';
 	}
 }
+
+const containerSearch = document.querySelector('.js-searchList');
 
 // FUNCION PARA PINTAR BÚSQUEDA EN HTML:
 function paintSearch() {
 	let liSearch = '';
+	liSearch += `<h2 class="searchList__title">Tu búsqueda</h2>`;
 	for (let i = 0; i < searchArray.length; i++) {
 		liSearch += `<li class="js-searchItem searchList__searchItem" data-id="${searchArray[i].showId}">`;
 		liSearch += `<h3 class="js-searchItemTitle searchList__searchItem--title">${searchArray[i].showName}</h3>`;
@@ -78,7 +90,7 @@ function addFavEvent() {
 	}
 }
 
-// FUNCIÓN PARA AÑADIR A FAV:
+// FUNCIÓN PARA MANEJAR FAVORITOS:
 function handleFav(event) {
 	const idSearch = event.currentTarget.dataset.id;
 	const titleSearch = event.currentTarget.querySelector('.js-searchItemTitle');
@@ -88,6 +100,7 @@ function handleFav(event) {
 		favArray = favArray.filter(
 			(eachFavItem) => eachFavItem.showId !== idSearch
 		);
+		paintFav();
 	} else {
 		event.currentTarget.classList.add('addFav');
 		favArray.push(
@@ -99,5 +112,35 @@ function handleFav(event) {
 				`Cartel de ${titleSearch.innerHTML}`
 			)
 		);
+		paintFav();
 	}
 }
+
+const containerFav = document.querySelector('.js-favList');
+
+// FUNCION PARA PINTAR FAVORITOS EN HTML:
+function paintFav() {
+	let liFav = '';
+	for (let i = 0; i < favArray.length; i++) {
+		liFav += `<li class="js-favItem favList__favItem" data-id="${favArray[i].showId}">`;
+		liFav += `<h3 class="js-favItemTitle favList__favItem--title">${favArray[i].showName}</h3>`;
+		liFav += `<img class="js-favItemImg favList__favItem--img" src="${favArray[i].showImage}" alt="Imagen del cartel de ${favArray[i].showAlt}" title="Cartel de ${favArray[i].showTitle}"/>`;
+		liFav += `</li>`;
+		containerFav.innerHTML = liFav;
+	}
+}
+
+// MEJORA:  FUNCION PARA AVISAR DE FAVORITOS VACÍO:
+// const emptySearch = document.querySelector('.js-favListEmpty');
+// const adviceSearch = document.querySelector('.js-favListAdvice');
+// function emptyFav() {
+// 	if (liFav.length < 1) {
+// 		console.log('entro en IF para quiter texto de favorito vacio');
+// 		emptySearch.classList.remove('hidden');
+// 		adviceSearch.classList.remove('hidden');
+// 	} else {
+// 		emptySearch.classList.add('hidden');
+// 		adviceSearch.classList.add('hidden');
+// 	}
+// }
+// emptyFav();
